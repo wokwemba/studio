8736d03aa494f2b7cfe7877b0cc86461f62e96ab
Washington Okwemba <tonywash87@gmail.com>
2026-01-07 20:10:26 +0000

    /signup exits after some seconds

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,17 @@
 
 import { NextResponse, type NextRequest } from 'next/server';
-import type { User } from 'firebase/auth';
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   const session = request.cookies.get('firebase-session-token');
+  const isProtectedRoute = request.nextUrl.pathname.startsWith('/admin') || request.nextUrl.pathname.startsWith('/profile');
 
-  // If no session token and trying to access a protected route, redirect to login
-  if (!session && request.nextUrl.pathname.startsWith('/admin')) {
-    return NextResponse.redirect(new URL('/login', request.url));
-  }
-  
-  if (!session && request.nextUrl.pathname.startsWith('/profile')) {
+  // If trying to access a protected route without a session token, redirect to login
+  if (isProtectedRoute && !session) {
     return NextResponse.redirect(new URL('/login', request.url));
   }
 
-  // If there is a session, continue
+  // If there is a session or the route is not protected, continue
   return NextResponse.next();
 }
 
