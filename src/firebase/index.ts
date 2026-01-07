ffd8a78ddb218cdeeed4378a2eb5cc63ebf332d0
Washington Okwemba <tonywash87@gmail.com>
2026-01-07 20:37:02 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -7,28 +7,27 @@ import { Firestore, getFirestore } from 'firebase/firestore'
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
-  if (!getApps().length) {
-    // Important! initializeApp() is called without any arguments because Firebase App Hosting
-    // integrates with the initializeApp() function to provide the environment variables needed to
-    // populate the FirebaseOptions in production. It is critical that we attempt to call initializeApp()
-    // without arguments.
-    let firebaseApp;
-    try {
-      // Attempt to initialize via Firebase App Hosting environment variables
-      firebaseApp = initializeApp();
-    } catch (e) {
-      // Only warn in production because it's normal to use the firebaseConfig to initialize
-      // during development
-      if (process.env.NODE_ENV === "production") {
-        console.warn('Automatic initialization failed. Falling back to firebase config object.', e);
-      }
-      firebaseApp = initializeApp(firebaseConfig);
-    }
-
+  if (getApps().length) {
     return getSdks();
   }
 
-  // If already initialized, return the SDKs with the already initialized App
+  // Important! initializeApp() is called without any arguments because Firebase App Hosting
+  // integrates with the initializeApp() function to provide the environment variables needed to
+  // populate the FirebaseOptions in production. It is critical that we attempt to call initializeApp()
+  // without arguments.
+  let firebaseApp;
+  try {
+    // Attempt to initialize via Firebase App Hosting environment variables
+    firebaseApp = initializeApp();
+  } catch (e) {
+    // Only warn in production because it's normal to use the firebaseConfig to initialize
+    // during development
+    if (process.env.NODE_ENV === "production") {
+      console.warn('Automatic initialization failed. Falling back to firebase config object.', e);
+    }
+    firebaseApp = initializeApp(firebaseConfig);
+  }
+
   return getSdks();
 }
 
