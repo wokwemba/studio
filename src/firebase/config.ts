00dfaf6116be188f93480fc6f6cd902b77f21c9d
Washington Okwemba <tonywash87@gmail.com>
2026-01-16 20:57:49 +0000

    Update Firebase Config in Your App Your firebase/index.ts should use the

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   "projectId": "studio-9516339656-c2f9c",
   "appId": "1:482661184998:web:864c46195b1e9d40dd1227",
   "apiKey": "AIzaSyDAzjugbkZR8z_gNEo8zQGZGcgZi_oR2qs",
-  "authDomain": "studio-9516339656-c2f9c.firebaseapp.com",
+  "authDomain": "studio--studio-9516339656-c2f9c.us-central1.hosted.app",
   "measurementId": "",
   "messagingSenderId": "482661184998"
 };
