b68e9da21bfe6f6bed58b4cc52aeac073f6614b0
Washington Okwemba <tonywash87@gmail.com>
2026-01-18 18:52:55 +0000

    Try fixing this error: `Console Error: In HTML, <div> cannot be a descen

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -25,12 +25,12 @@ const badgeVariants = cva(
 )
 
 export interface BadgeProps
-  extends React.HTMLAttributes<HTMLDivElement>,
+  extends React.HTMLAttributes<HTMLSpanElement>,
     VariantProps<typeof badgeVariants> {}
 
 function Badge({ className, variant, ...props }: BadgeProps) {
   return (
-    <div className={cn(badgeVariants({ variant }), className, 'font-mono uppercase')} {...props} />
+    <span className={cn(badgeVariants({ variant }), className, 'font-mono uppercase')} {...props} />
   )
 }
 
