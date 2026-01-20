865d41573d216e7e8be14f8a3b755005d829422c
Washington Okwemba <tonywash87@gmail.com>
2026-01-20 20:17:41 +0000

    google auth is failing to populate the pop up to select gmail account.it

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   "projectId": "studio-9516339656-c2f9c",
   "appId": "1:482661184998:web:864c46195b1e9d40dd1227",
   "apiKey": "AIzaSyDAzjugbkZR8z_gNEo8zQGZGcgZi_oR2qs",
-  "authDomain": "studio--studio-9516339656-c2f9c.us-central1.hosted.app",
+  "authDomain": "studio-9516339656-c2f9c.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "482661184998"
 };
