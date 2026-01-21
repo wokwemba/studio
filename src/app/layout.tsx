c176c568064000c25f6657ce984e067c6f128083
Washington Okwemba <tonywash87@gmail.com>
2026-01-21 09:03:35 +0000

    Couldn't verify your site We couldn't verify your site. Make sure the ch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,8 +26,6 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap" rel="stylesheet" />
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7698959644327699"
-     crossOrigin="anonymous"></script>
       </head>
       <body className={cn('font-body antialiased')} suppressHydrationWarning>
         <FirebaseClientProvider>
